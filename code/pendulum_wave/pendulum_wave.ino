29fcc4f18f3294e1ddada7547cef5dc0f9783865
Johan Beyers <johan@juizi.com>
2017-12-03 11:00:46 +0200

    Working with spaced pulsing

--- code/pendulum_wave/pendulum_wave.ino
+++ code/pendulum_wave/pendulum_wave.ino
@@ -16,13 +16,16 @@ unsigned long expected_triggers[pendulums]; // When we expect the next positive 
 unsigned long previous_triggers[pendulums]; // Keep the previous swing trigger timestamps for imbalance calculations.
 unsigned long previous_triggers_2[pendulums]; // Keep the previous swing trigger timestamps for imbalance calculations.
 int trigger_counts[pendulums]; // Count positive triggers to implement a state machine
+bool should_pulse[pendulums];
 int imbalances[pendulums]; // Track imbalance in the swing length.
 bool pos[pendulums]; // flag if this is a positive or negative swing direction. positive is the one taking longer.
 float where; // Computes where in the cycle we are.
 float modulo; // computes where in the swing we are.
 int i; // Loop iterator
+int j; // Loop iterator
 int pendulum_to_view = -1; //Which pendulum to view
 unsigned long switch_time;
+int min_counts;
 
 void setup() {
   Serial.begin(9600);
@@ -40,6 +43,9 @@ void setup() {
     // Initial values for average periods
     average_periods[i] = 60000ul / (long(first_tempo) - long(i));
 
+    // Trigger counts
+    trigger_counts[i] = 0;
+
     // Set the output pins
     pinMode(i + 2, OUTPUT);
   }
@@ -49,6 +55,36 @@ void setup() {
 // delay, 1 for the main code loop.
 void loop() {
   now = millis();
+  if ( now - switch_time > 2001 ) {
+    switch_time = now;
+    pendulum_to_view += 1;
+    if (pendulum_to_view > 7) {
+      pendulum_to_view = 0;
+    }
+    Serial.print(pendulum_to_view);
+    Serial.print(" | where = ");
+    Serial.print(where);
+    Serial.print(" | imb= ");
+    Serial.print(imbalances[pendulum_to_view]);
+    Serial.print(" | ideal = ");
+    Serial.print(ideal_periods[pendulum_to_view]);
+    Serial.print(" | actual = ");
+    Serial.print(average_periods[pendulum_to_view]);
+    Serial.println();
+    Serial.print("triggers: ");
+    for ( j = 0; j < pendulums; j++ ) {
+      Serial.print(" | ");
+      Serial.print(expected_triggers[j]);
+    }
+    Serial.println();
+    Serial.print("pulses  : ");
+    for ( j = 0; j < pendulums; j++ ) {
+      Serial.print(" | ");
+      Serial.print(pulses[j]);
+    }
+    Serial.println();
+  }
+
   for ( i = 0; i < pendulums; i++ ) {
 
     // Read the sensor
@@ -67,14 +103,17 @@ void loop() {
 
     // Trigger detection of a magnet entering
     if ((sensors[i] > (averages[i] + trigger_level)) && (lockouts[i] < now )) {
+
       // Reset the lockout
-      lockouts[i] = now + (unsigned long)(ideal_periods[i] * 0.4);
+      lockouts[i] = now + long(ideal_periods[i] * 0.4);
 
       // Change swing direction.
       pos[i] = !pos[i];
 
-      // Tally the swing imbalance and if it's the reverse of what we expect, reverse the swing directions.
+      // The triggers trigger every time the magnet passes the coil. We only want to work on the positive swing.
       if (pos[i]) {
+
+        // Tally the swing imbalance and if it's the reverse of what we expect, reverse the swing directions.
         if (now - previous_triggers[i] >= previous_triggers[i] - previous_triggers_2[i]) {
           if (imbalances[i] < 30) {
             imbalances[i]++;
@@ -86,69 +125,54 @@ void loop() {
             pos[i] = !pos[i];
           }
         }
+
         // Where in the cycle should we be?
         where = (now % 60000ul)/ideal_periods[i];
         modulo = where - (int)where;
 
-        // Only pulse if the speed is too slow or if we are behind
-        // TODO: This is overly simplistic
+        // Only pulse if the speed is too slow and we are behind
         if (modulo < 0.85 && ideal_periods[i] <= float(now - previous_triggers_2[i] + min(4ul, long(10.0 * modulo)))) {
-          if ( pendulum_to_view < 0 && switch_time < now ) {
-            pendulum_to_view = i;
-          }
-          if ( i == pendulum_to_view ) {
-            pulses[i] = now + pulse;
-            digitalWrite(i + 2, HIGH);
-
-            Serial.print(i);
-            Serial.print(" | where = ");
-            Serial.print(where);
-            Serial.print(" | imbalance = ");
-            Serial.print(imbalances[i]);
-            Serial.print(" | expected period = ");
-            Serial.print(ideal_periods[i]);
-            Serial.print(" | actual period = ");
-            Serial.print(now - previous_triggers_2[i]);
-            Serial.println();
-          }
+          should_pulse[i] = true;
         } else {
-          if (pendulum_to_view == i) {
-            pendulum_to_view = -1;
-            switch_time = now + lockout + 1111ul;
-            
-            Serial.print("sw | ");
-            Serial.print(i);
-            Serial.print(" | where = ");
-            Serial.print(where);
-            Serial.print(" | imbalance = ");
-            Serial.print(imbalances[i]);
-            Serial.print(" | expected period = ");
-            Serial.print(ideal_periods[i]);
-            Serial.print(" | actual period = ");
-            Serial.print(now - previous_triggers_2[i]);
-            Serial.println();
+          should_pulse[i] = false;
+        }
 
+        // Update the trigger counts
+        trigger_counts[i] = trigger_counts[i] + 1;
+
+        // Update the expected next pulse start and stop times only if we are not actively triggering
+        if (trigger_counts[i] > 2) {
+          expected_triggers[i] = now + average_periods[i];
+          if (should_pulse[i]) {
+            pulses[i] = expected_triggers[i] + pulse;
           }
         }
+        // Update the average periods
+        if (now - previous_triggers_2[i] < average_periods[i]) {
+          average_periods[i] -= 1ul;
+        } else if (now - previous_triggers_2[i] > average_periods[i]) {
+          average_periods[i] += 1ul;
+        }
       }
+
       // Update the previous trigger times
       previous_triggers_2[i] = previous_triggers[i];
       previous_triggers[i] = now;
 
-      // Update the average periods
-      if (now - previous_triggers_2[i] < average_periods[i]) {
-        average_periods[i] -= 1ul;
-      } else if (now - previous_triggers_2[i] > average_periods[i]) {
-        average_periods[i] += 1ul;
+    }
+
+    min_counts = 10;
+    for ( j = 0; j < pendulums; j++ ) {
+      min_counts = min(trigger_counts[j], min_counts);
+    }
+    if (min_counts > 2) {
+      for ( j = 0; j < pendulums; j++ ) {
+        trigger_counts[j] = 0;
       }
-
-      // Update the expected next trigger times
-      expected_triggers[i] = now + average_periods[i];
-
     }
 
     // Write out the pulse
-    if ( now < pulses[i] ) {
+    if ( now < pulses[i] && now >= expected_triggers[i] && trigger_counts[i] < 2) {
       digitalWrite(i + 2, HIGH);
     } else {
       digitalWrite(i + 2, LOW);
